aa51beb494f251fdb3d2add931ab683bc28d9e54
David Antognoli <david.antognoli@gmail.com>
2019-04-24 16:55:16 -0500

    adds project

---
+++ Assets/FancyObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FancyObject : MonoBehaviour
+{
+    [SerializeField]
+    private FancyObjectData fancyObjectData;
+
+    private int number1, number2;
+    private string string1, string2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        number1 = fancyObjectData.number1;
+        number2 = fancyObjectData.number2;
+        string1 = fancyObjectData.string1;
+        string2 = fancyObjectData.string2;
+
+        Debug.Log($"{number1}, {number2}, {string1}, {string2}");
+    }
+}
---
+++ Assets/FancyObjectData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+/// <summary>
+/// Data class used to provide raw data to a FancyObject.
+/// This can make setting up objects in the editor easier,
+/// and can be useful for saving data.
+/// 
+/// You can't serialize MonoBehaviours, so using these types of 
+/// data classes associated with your MonoBehaviour classes can help
+/// circumvent the problem.
+/// 
+/// Needs `using System;` to work!
+/// </summary>
+[Serializable]
+public class FancyObjectData
+{
+    public int number1, number2;
+    public string string1, string2;
+}
